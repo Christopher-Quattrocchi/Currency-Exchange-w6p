5055645da83bd882f483f03652c4b702c61ed6af
Christopher Quattrocchi <chrisquattrocchi@gmail.com>
2023-11-17 11:36:27 -0800

    dropdown works

---
+++ README.md
@@ -0,0 +1,17 @@
+Main objectives:
+
+Enter amount of USD, show conversion for at least five other currencies
+
+Error message for request status and for incorrect input
+
+If currency doesn't exist, say so in error
+
+Secondary objectives:
+
+Allow to convert between all currency types
+
+Allow to convert to and from US dollars
+
+*Cache api results so only one call is needed*
+
+Dropdown for currencies
--- src/index.js
+++ src/index.js
@@ -30,34 +30,34 @@ function printError(error, dollarAmount, currencyKey) {
   document.querySelector('#showResponse').innerText = `There was an error converting ${dollarAmount} USD to ${currencyKey}: ${error}.`;
 }
 
-function currencySelect(currency) {
-  let currencyKey;
-  if (currency === "EURO") {
-    currencyKey = "EUR";
-    return currencyKey;
-  } else if (currency === "POUND") {
-    currencyKey = "GBP";
-    return currency;
-  } else if (currency === "YEN") {
-    currencyKey = "JPY";
-    return currency;
-  } else if (currency === "FRANC") {
-    currencyKey = "CHF";
-    return currency;
-  } else if (currency === "RUPEE") {
-    currencyKey = "INR";
-    return currencyKey;
-  }
-}
+// function currencySelect(currency) {
+//   let currencyKey;
+//   if (currency === "EURO") {
+//     currencyKey = "EUR";
+//     return currencyKey;
+//   } else if (currency === "POUND") {
+//     currencyKey = "GBP";
+//     return currency;
+//   } else if (currency === "YEN") {
+//     currencyKey = "JPY";
+//     return currency;
+//   } else if (currency === "FRANC") {
+//     currencyKey = "CHF";
+//     return currency;
+//   } else if (currency === "RUPEE") {
+//     currencyKey = "INR";
+//     return currencyKey;
+//   }
+// }
 
 function handleFormSubmission(event) {
   event.preventDefault();
   // debugger;
   const dollarAmount = document.querySelector('#dollar-amount').value;
-  let currency = document.querySelector('#currency').value.toUpperCase();
+  let currencyKey = document.querySelector('#currency').value;
   document.querySelector('#dollar-amount').value = null;
-  document.querySelector('#currency').value = null;
-  let currencyKey = currencySelect(currency);
+  // document.querySelector('#currency').value = null;
+  // let currencyKey = currencySelect(currency);
   getCoin(dollarAmount, currencyKey);
 }
 
